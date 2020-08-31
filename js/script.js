1a4592ee4d757b07c954666d3fda7f341cb9717c
Doron <lisbona107@gmail.com>
2020-08-31 18:21:10 +0300

    first draft all website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".nav-tabs a").click(function(){
       const tabId = $(this)[0].hash;
+      
       if(tabId === "#big-flower"){
         console.log("big flower");
       }
