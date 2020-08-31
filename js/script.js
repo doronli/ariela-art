0fe4cd4013c8f798691c894a2c227d239e2dff7f
Doron <lisbona107@gmail.com>
2020-08-31 15:42:05 +0300

    seashell tab finish

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+      const tabId = $(this)[0].hash;
+      if(tabId === "#big-flower"){
+        console.log("big flower");
+      }
+      else if(tabId === "#small-flower"){
+        console.log("small flower");
+
+      }
+      else { // seashell
+        console.log("seashell");
+
+      }
+    });
+  });
